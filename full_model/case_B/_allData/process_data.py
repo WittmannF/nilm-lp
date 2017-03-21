afc7a0d603c83d6ede7a7f5e5c0c34457535ff3c
Fernando Marcos Wittmann <Fernando Marcos Wittmann>
2017-03-21 14:47:45 -0300

    Add Bayesian Gaussian Mixture model

--- full_model/case_B/_allData/process_data.py
+++ full_model/case_B/_allData/process_data.py
@@ -24,7 +24,7 @@ d = dict(zip(names,data))
 
 # Plot by size 
 init = 0
-end = 5000
+end = 50000
 
 # Sum all active power, except WHE 
 all_P = sum(v.P for (k,v) in d.items() if k!='WHE')
@@ -37,6 +37,7 @@ for appl in names:
 plt.legend(names)
 
 # Plot clusters of active and reactive power
+plt.figure()
 for appl in names:
     if appl != 'WHE':
         plt.plot(d[appl].P[init:end], d[appl].Q[init:end],'o', alpha=0.1)
@@ -45,7 +46,7 @@ to_leg = [n for n in names if n!='WHE']
 to_leg.append('WHE Steps')
 
 # Get steps
-w = 5
+w = 1
 P_steps = all_P.values[init+w:end] - all_P.values[init:end-w]
 Q_steps = all_Q.values[init+w:end] - all_Q.values[init:end-w]
 
@@ -61,9 +62,7 @@ plt.legend(to_leg)
 
 # Visualize appliance for test clustering 
 init = 0
-end = 43200 # minutes
-appl = 'BME'
-# plt.plot(d[appl].P[init:end], d[appl].Q[init:end],'o', alpha=0.1)
+end = 43200 # minutes = 1 month
 
 # Import clusterer
 from sklearn.cluster import DBSCAN
@@ -74,6 +73,10 @@ from numpy import linalg as LA
 colors = np.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
 colors = np.hstack([colors] * 20)
 
+## Test DBSCAN for one appliance
+appl = 'BME'
+# plt.plot(d[appl].P[init:end], d[appl].Q[init:end],'o', alpha=0.1)
+
 # Compute DBSCAN
 X = np.transpose([d[appl].P[init:end].values, d[appl].Q[init:end].values])
 X = StandardScaler().fit_transform(X)
@@ -82,11 +85,9 @@ y_pred = y_pred = db.labels_.astype(np.int)
 plt.figure()
 plt.scatter(X[:,0],X[:,1], color=colors[y_pred])
 
-# Plot multiple graphs with clusterers
+## Test DBSCAN for all appliances
 delta = 30 # VA (minimum aparent power to be identified)
 for appl in names:
-    if appl == 'B2E':
-        continue
     # Create vector with P and Q values
     P = d[appl].P[init:end].values
     Q = d[appl].Q[init:end].values
@@ -103,7 +104,7 @@ for appl in names:
     
     # Apply clusterer
     db = DBSCAN(eps=1, min_samples=50).fit(X)
-    y_pred = y_pred = db.labels_.astype(np.int)
+    y_pred = db.labels_.astype(np.int)
 
     # Plot clusters with X unnormalized
     X = sscl.inverse_transform(X)    
@@ -111,9 +112,63 @@ for appl in names:
     plt.scatter(X[:,0],X[:,1], color=colors[y_pred])
     
 
+# TODO: Test Baysean Gaussian Mixture clustering
 
+# Visualize appliance for test clustering 
+init = 0
+end = 5000 # minutes
 
+# Import clusterer
+from sklearn.mixture import BayesianGaussianMixture
 
+# Set list of colors
+colors = np.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
+colors = np.hstack([colors] * 20)
+
+## Test DBSCAN for one appliance
+appl = 'BME'
+plt.plot(d[appl].P[init:end], d[appl].Q[init:end],'o', alpha=0.1)
+
+# Compute BGM
+X = np.transpose([d[appl].P[init:end].values, d[appl].Q[init:end].values])
+X = StandardScaler().fit_transform(X)
+bgm = BayesianGaussianMixture(n_components=4, covariance_type='full', weight_concentration_prior_type='dirichlet_distribution', random_state=42).fit(X)
+y_pred = bgm.predict(X)
+plt.figure()
+plt.scatter(X[:,0],X[:,1], color=colors[y_pred])
+means = bgm.means_
+plt.plot(means[:,0],means[:,1],'kx')
+
+## Test BGM for all appliances
+delta = 30 # VA (minimum aparent power to be identified)
+
+for appl in names:
+    # Create vector with P and Q values
+    P = d[appl].P[init:end].values
+    Q = d[appl].Q[init:end].values
+    X = np.transpose([P, Q])
+    X = [x for x in X if LA.norm(x)>=delta] # Ignore values lower than delta VA
+
+    # Normalize X
+    if len(X) == 0:
+        print 'No value lower than {} VA was found in {}'.format(delta, appl)
+        continue
+    
+    sscl = StandardScaler().fit(X)
+    X = sscl.transform(X)
+    
+    # Apply clusterer
+    bgm = BayesianGaussianMixture(n_components=3, covariance_type='full', weight_concentration_prior_type='dirichlet_distribution', random_state=42).fit(X)
+    y_pred = bgm.predict(X)
+
+    # Plot clusters with X unnormalized
+    X = sscl.inverse_transform(X)    
+    plt.figure()
+    plt.scatter(X[:,0],X[:,1], color=colors[y_pred], alpha=0.5)
+    means = sscl.inverse_transform(bgm.means_)
+    plt.plot(means[:,0],means[:,1],'kx')
+
+    
 
 
 
