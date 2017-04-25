087cf96cffae2731a819ce6371363753b959d440
Fernando Marcos Wittmann <fernando.wittmann@gmail.com>
2017-04-25 14:42:29 -0300

    Create function to generate ground truth data in CSV

--- full_model/case_B/_allData/convert_files.py
+++ full_model/case_B/_allData/convert_files.py
@@ -5,4 +5,24 @@ Created on Tue Apr 25 14:10:31 2017
 @author: wittmann
 """
 
-g
+import numpy as np
+import pandas as pd
+
+def get_PQ(app_list):
+    data = [pd.read_csv(f+'.csv') for f in app_list]
+    
+    all_P = sum(d.P for d in data)
+    all_Q = sum(d.Q for d in data)
+    
+    return[all_P, all_Q]
+
+def create_ground_truth(app_list, init, ending, path):
+    data = [pd.read_csv(f+'.csv') for f in app_list]
+    
+    appl_P = np.transpose([d.P.values[init-1:ending] for d in data])
+    ground_truth = pd.DataFrame(appl_P, columns=app_list, index=range(init,ending+1))
+    
+    ground_truth['all_P'] = ground_truth.sum(1)
+    
+    ground_truth.to_csv(path)
+    
--- full_model/case_B/_allData/main.py
+++ full_model/case_B/_allData/main.py
@@ -5,4 +5,10 @@ Created on Tue Apr 25 14:11:26 2017
 @author: wittmann
 """
 
-f
+from convert_files import create_ground_truth
+
+app_list = app_list = ['BME', 'CDE', 'DWE', 'FGE', 'FRE', 'HPE', 'TVE']
+init = 1
+ending = 10080
+
+create_ground_truth(app_list, init, ending, '../z_ground_truth.csv')
