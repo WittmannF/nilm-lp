0be734201f24693789e8ca90d03ed5bb2e1fe39e
Fernando Marcos Wittmann <fernando.wittmann@gmail.com>
2017-04-25 14:12:49 -0300

    Create py files for CSV conversion

---
+++ full_model/case_B/_allData/convert_files.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 25 14:10:31 2017
+
+@author: wittmann
+"""
+
+g
---
+++ full_model/case_B/_allData/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 25 14:11:26 2017
+
+@author: wittmann
+"""
+
+f
